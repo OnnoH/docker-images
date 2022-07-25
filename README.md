cca2515b98eb4b6cd9717ad65f7521be995e9fc0
OnnoH <ohuijgen@gmail.com>
2022-07-25 20:31:36 +0000

    Added volume to node-red container.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # docker-images
+
+These Dockerfile will create images for machines with an `arm32v7` architecture.
+
+They're based on Alpine Linux.
+
--- node-red/start-red.sh
+++ node-red/start-red.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --detach --name red-frederik --restart=always --publish 1880:1880 arm32v7/node-red:3.0.1
+docker run --detach --name red-frederik --restart=always --publish 1880:1880 --volume ${HOME}/node-red-data arm32v7/node-red:3.0.1
